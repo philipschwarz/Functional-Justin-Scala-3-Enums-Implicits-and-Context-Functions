a90cca84ed5c6a259c47aac6d0a9353391dbf01b
philipjss <philip.schwarz@sefaira.com>
2020-11-21 19:33:34 +0000

    replace object with main method

--- src/main/scala/Scala2Eval.scala
+++ src/main/scala/Scala2Eval.scala
@@ -1,31 +1,31 @@
-object Scala2Eval extends App:
-  
-  // An expression evaluator using Scala 2
+// An expression evaluator 
+@main def scala2Eval: Unit =
 
   type Env = Map[String, Int]
   type WithEnv = Env ?=> Int
-  
+
   enum Exp:
     case Val(value: Int)
     case Add(left: Exp, right: Exp)
     case Mul(left: Exp, right: Exp)
     case Var(identifier: String)
-  
+
   import Exp._
-  def eval(exp: Exp): WithEnv  = 
+
+  def eval(exp: Exp): WithEnv =
     exp match {
       case Var(id) => handleVar(id)
       case Val(value) => value
-      case Add(l,r) => handleAdd(l,r)
-      case Mul(l,r) => handleMul(l,r)
+      case Add(l, r) => handleAdd(l, r)
+      case Mul(l, r) => handleMul(l, r)
     }
-  
+
   def summonEnv: Env ?=> Env = summon[Env]
   
   def handleAdd(l: Exp, r: Exp): WithEnv = eval(l) + eval(r)
   def handleMul(l: Exp, r: Exp): WithEnv = eval(l) * eval(r)
-  def handleVar(s: String): WithEnv = 
-    summonEnv.getOrElse(s,0)
+  def handleVar(s: String): WithEnv =
+    summonEnv.getOrElse(s, 0)
 
   val exp1: Exp =
     Mul(
@@ -37,10 +37,10 @@ object Scala2Eval extends App:
           Var("y"))
       )
     )
-    
-  given env as Env = Map( "x" -> 17, "y" -> 10, "z" -> 2)
+
+  given env as Env = Map("x" -> 17, "y" -> 10, "z" -> 2)
   
   val eval1 = eval(exp1)
-  
-  assert( eval1 == 400 )
+
+  assert(eval1 == 400)
   println(s"Eval exp gives $eval1")
